817d15a04e89ea480a56302b7862726ce109b1b6
Govind Patil <Govind Patil@192.168.1.80>
2023-05-27 18:52:09 +0530

    commit.....

---
+++ src/main/java/com/simple/registration/Registrations.java
@@ -0,0 +1,69 @@
+package com.simple.registration;
+
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+/**
+ * Servlet implementation class Registrations
+ */
+@WebServlet("/registrations")
+public class Registrations extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+		String uname=request.getParameter("name");
+		String umail=request.getParameter("email");
+		String upwd=request.getParameter("pass");
+		String umobile=request.getParameter("contact");
+		RequestDispatcher  dispatcher=null;
+		Connection con=null;
+		try
+		{
+			Class.forName("com.mysql.cj.jdbc.Driver");
+			con=DriverManager.getConnection("jdbc:mysql://localhost:3306/customer?useSSL=false","root","Raj@12345");
+			PreparedStatement  pst=con.prepareStatement(" insert into login (uname , upwd , umail, umobile) values(? ,? ,? ,?) ");
+			pst.setString(1, uname);
+			pst.setString(2, upwd);
+			pst.setString(3, umail);
+			pst.setString(4, umobile);
+			
+			int rowCount=pst.executeUpdate();
+			dispatcher =request.getRequestDispatcher("registration.jsp");
+			
+			if(rowCount > 0)
+			{
+				request.setAttribute("status", "success");
+			}
+			else
+			{
+				request.setAttribute("status", "Failed");
+			}
+			dispatcher.forward(request, response);
+		}
+		catch(Exception e)
+		{
+			e.printStackTrace();
+		}
+		finally
+		{
+			try {
+				con.close();
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+
+}
